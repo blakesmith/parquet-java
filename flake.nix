425d5fc23af29b853372ca03b505a044d0e285ed
Blake Smith <blake@sproutsocial.com>
2024-09-25 17:11:46 -0500

    Add development flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = with pkgs; {
+          default = mkShell {
+            buildInputs = [
+              thrift
+              protobuf
+              maven
+              jdk8
+            ];
+          };
+        };
+      }
+    );
+}
